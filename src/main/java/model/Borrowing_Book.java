81c80873832ecc22d7e0f32eff742804514e5ea3
LeDuyKhanh1989 <designer.expert228@gmail.com>
2023-08-30 13:48:00 +0700

    reupdate model - Done!

--- src/main/java/model/Borrowing_Book.java
+++ src/main/java/model/Borrowing_Book.java
@@ -1,2 +1,37 @@
-package model;public class Borrowing_Book {
+package model;
+
+public class Borrowing_Book {
+    private int id;
+    private Book book;
+    private String status;
+
+    public Borrowing_Book(int id, Book book, String status) {
+        this.id = id;
+        this.book = book;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
