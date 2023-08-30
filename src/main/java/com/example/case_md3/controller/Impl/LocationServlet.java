2b6699dca3a3bd1aae78cde9660d2ac63f73bf5b
chien2002 <chienad2002ad@gmail.com>
2023-08-30 16:51:56 +0700

    location

--- src/main/java/com/example/case_md3/controller/Impl/LocationServlet.java
+++ src/main/java/com/example/case_md3/controller/Impl/LocationServlet.java
@@ -1,4 +1,18 @@
 package com.example.case_md3.controller.Impl;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
 
-public class LocationServlet {
+@WebServlet(name = "LocationServlet", value = "/LocationServlet")
+public class LocationServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
 }
